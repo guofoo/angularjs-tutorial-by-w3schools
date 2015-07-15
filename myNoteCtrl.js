a23b5283ba86162ffc90f3f050f7a6e136cd3451
live4code <live4code@gmail.com>
2015-07-15 07:36:49 +0000

    commit exercise content by Live4Code

---
+++ myNoteCtrl.js
@@ -0,0 +1,13 @@
+app.controller("myNoteCtrl", function($scope) {
+    $scope.message = "";
+    
+    $scope.left  = function() {
+      
+    };
+    
+    $scope.clear = function() {
+      
+    };
+    
+    $scope.save  = function() { alert("Note Saved"); };
+});
