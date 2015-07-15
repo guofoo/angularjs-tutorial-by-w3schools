16e5249ab983b00f5a299c0f4fc26c6fde256109
live4code <live4code@gmail.com>
2015-07-15 06:50:47 +0000

    commit exercise content by Live4Code

--- myUsers.js
+++ myUsers.js
@@ -4,12 +4,12 @@ $scope.lName = '';
 $scope.passw1 = '';
 $scope.passw2 = '';
 $scope.users = [
-{id:1, fName:'Hege',  lName:"Pege" },
-{id:2, fName:'Kim',   lName:"Pim" },
-{id:3, fName:'Sal',   lName:"Smith" },
-{id:4, fName:'Jack',  lName:"Jones" },
-{id:5, fName:'John',  lName:"Doe" },
-{id:6, fName:'Peter', lName:"Pan" }
+{id:1, fName:'Hege',  lName:'Pege' },
+{id:2, fName:'Kim',   lName:'Pim' },
+{id:3, fName:'Sal',   lName:'Smith' },
+{id:4, fName:'Jack',  lName:'Jones' },
+{id:5, fName:'John',  lName:'Doe' },
+{id:6, fName:'Peter', lName:'Pan' }
 ];
 $scope.edit = true;
 $scope.error = false;
